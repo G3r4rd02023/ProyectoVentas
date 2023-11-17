ef00b2c31ec04954ec86f6bdf7d633bcd758073f
Gerardo Lanza <glanza007@gmail.com>
2023-11-17 16:17:10 -0600

    Ver detalles de producto

---
+++ ProyectoVentas/Controllers/CatalogoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoVentas.Models;
+
+namespace ProyectoVentas.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private readonly VentasContext _context;
+
+        public CatalogoController(VentasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Productos.ToListAsync());
+        }
+
+        public async Task<IActionResult> Detalles(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+    }
+}
