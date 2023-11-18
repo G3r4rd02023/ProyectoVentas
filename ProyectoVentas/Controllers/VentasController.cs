f8aacf4a39ce031be44305dcec7a7a0095e34387
Gerardo Lanza <glanza007@gmail.com>
2023-11-18 10:13:22 -0600

    Procesar compra

---
+++ ProyectoVentas/Controllers/VentasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoVentas.Models;
+using ProyectoVentas.Models.Entidades;
+
+namespace ProyectoVentas.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly VentasContext _context;
+
+        public VentasController(VentasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DetalleVentas
+                .Include(sd => sd.Venta)                
+                .ThenInclude(sd => sd.Usuario)
+                .Include(sd => sd.Producto)
+                .ToListAsync());
+        }
+
+        public async Task<IActionResult> Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            DetalleVenta detalle = await _context.DetalleVentas
+                .Include(sd => sd.Venta)
+                .ThenInclude(sd => sd.Usuario)
+                .Include(sd => sd.Producto)                
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (detalle == null)
+            {
+                return NotFound();
+            }
+
+            return View(detalle);
+        }
+
+    }
+}
--- ProyectoVentas/Models/VentaViewModel.cs
+++ ProyectoVentas/Models/VentaViewModel.cs
@@ -3,9 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoVentas.Models
 {
-    public class VentaViewModel
+    public class VentaViewModel : Venta
     {
-        public ICollection<DetalleVenta> DetalleVentas { get; set; }
+       
         public Producto Producto { get; set; }
         public int  ProductoId { get; set; }
         public decimal Precio { get; set; }
@@ -16,6 +16,6 @@ namespace ProyectoVentas.Models
         public int Cantidad { get; set; }
 
         public DateTime Fecha { get; set; } = DateTime.Now;
-        public Usuario Usuario { get; set; }
+       
     }
 }
